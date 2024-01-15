bb6a4ef885c1fa3c6c9bafae140974293462758f
Simon Willison <swillison@gmail.com>
2024-01-14 17:25:19 -0800

    Tests for CSV and JSON, refs #8, #7

--- tests/test_build.py
+++ tests/test_build.py
@@ -44,3 +44,51 @@ def test_demo_db_tsv(demo_db):
         "name": "Egypt",
         "population": 102334404,
     }
+
+
+def test_demo_db_csv(demo_db):
+    cities = demo_db["cities"]
+    assert cities.count == 10
+    assert cities.columns_dict == {
+        "id": str,
+        "name": str,
+        "latitude": float,
+        "longitude": float,
+        "country": str,
+    }
+    # Spot check
+    assert next(cities.rows_where("id = 'nyc'")) == {
+        "id": "nyc",
+        "name": "New York City",
+        "latitude": 40.7128,
+        "longitude": -74.006,
+        "country": "US",
+    }
+    assert next(cities.rows_where("id = 'syd'")) == {
+        "id": "syd",
+        "name": "Sydney",
+        "latitude": -33.8688,
+        "longitude": 151.2093,
+        "country": "AU",
+    }
+
+
+def test_demo_db_json(demo_db):
+    museums = demo_db["museums"]
+    assert museums.count == 10
+    assert museums.columns_dict == {
+        "id": int,
+        "name": str,
+        "city_id": str,
+    }
+    # Spot check
+    assert next(museums.rows_where("id = 1")) == {
+        "id": 1,
+        "name": "Metropolitan Museum of Art",
+        "city_id": "nyc",
+    }
+    assert next(museums.rows_where("id = 10")) == {
+        "id": 10,
+        "name": "Chhatrapati Shivaji Maharaj Vastu Sangrahalaya",
+        "city_id": "mum",
+    }
